4a32e2d09fce6c7a2d08b364ec0c6bddb4aff8c1
Fowad Sohail <sohailf7@students.rowan.edu>
2020-11-19 15:43:27 -0500

    ahhh

--- main.py
+++ main.py
@@ -1,17 +1,54 @@
 import tensorflow as tf
+from tensorflow.keras import Model
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.losses import categorical_crossentropy
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, AveragePooling2D
+from tensorflow.keras.utils import to_categorical
+import numpy as np
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
+
+# Add a new axis
+x_train = x_train[:, :, :, np.newaxis]
+x_test = x_test[:, :, :, np.newaxis]
+
+input_shape = (28, 28, 1)
+num_classes = 10
+
+y_train = to_categorical(y_train, num_classes)
+y_test = to_categorical(y_test, num_classes)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+print(x_train.shape)
+print(y_train.shape)
+print(x_test.shape)
+print(y_test.shape)
 
 
-model = tf.keras.applications.EfficientNetB7(
-    include_top=True, weights='imagenet', input_tensor=None, input_shape=None,
-    pooling=None, classes=1000, classifier_activation='softmax'
-)
+# LeNet-5 model
+class LeNet(Sequential):
+    def __init__(self, input_shape, nb_classes):
+        super().__init__()
 
-# data_dir = tf.keras.utils.get_file(origin='/data/pytorch/',
-#                                    fname='cifar-10-python.tar.gz', 
-#                                    untar=True)
-data_dir = tf.keras.utils.get_file(origin='file:///data/pytorch/cifar-10-python.tar.gz',
-                                   fname='cifar-10-python', 
-                                   untar=True)
+        self.add(Conv2D(6, kernel_size=(5, 5), strides=(1, 1), activation='tanh', input_shape=input_shape, padding="same"))
+        self.add(AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid'))
+        self.add(Conv2D(16, kernel_size=(5, 5), strides=(1, 1), activation='tanh', padding='valid'))
+        self.add(AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid'))
+        self.add(Flatten())
+        self.add(Dense(120, activation='tanh'))
+        self.add(Dense(84, activation='tanh'))
+        self.add(Dense(nb_classes, activation='softmax'))
 
-print(model)
-print(data_dir)
+        self.compile(optimizer='adam',
+                    loss=categorical_crossentropy,
+                    metrics=['accuracy'])
+
+model = LeNet(input_shape, num_classes)
+model.summary()
+
+model.fit(x_train, y=y_train, 
+          epochs=20, 
+          validation_data=(x_test, y_test))
