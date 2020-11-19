88f73ceff5a8f6d1adb7992d3d3131dd2f43b3eb
Fowad Sohail <sohailf7@students.rowan.edu>
2020-11-19 14:58:38 -0500

    path

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ model = tf.keras.applications.EfficientNetB7(
     pooling=None, classes=1000, classifier_activation='softmax'
 )
 
-data_dir = tf.keras.utils.get_file(origin='/data/pytorch/',
-                                   fname='cifar-10-python.tar.gz', 
+# data_dir = tf.keras.utils.get_file(origin='/data/pytorch/',
+#                                    fname='cifar-10-python.tar.gz', 
+#                                    untar=True)
+data_dir = tf.keras.utils.get_file(origin='file:///data/pytorch/cifar-10-python.tar.gz',
+                                   fname='cifar-10-python', 
                                    untar=True)
 
 print(model)
