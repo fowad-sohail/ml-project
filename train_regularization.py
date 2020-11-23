f23e87db981af79565c3c8c6c1a1c33e2020e1bd
Spencer Hoffman <hoffmans1@students.rowan.edu>
2020-11-23 13:46:41 -0500

    Now trains the base and adversarial models, generates adversarial examples from the base model, and uses them to attack both.

--- train_regularization.py
+++ train_regularization.py
@@ -8,10 +8,18 @@ import numpy as np
 import neural_structured_learning as nsl
 from cleverhans.future.tf2.attacks import fast_gradient_method
 
+# Hyperparameters
+epochs = 20
+adv_mult = 0.2
+adv_step = 0.05
+attack_eps = 0.3
+
 # Load Fashion-MNIST Dataset
 input_shape = (28, 28)
 num_classes = 10
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
+y_train_cat = to_categorical(y_train, num_classes)
+y_test_cat = to_categorical(y_test, num_classes)
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 y_train = y_train.astype('float32')
@@ -38,22 +46,32 @@ def lenet_sequential(nb_classes, input_shape):
 		Dense(84, activation='relu'),
 		Dense(nb_classes, activation='softmax')
 	])
-	adv_config = nsl.configs.make_adv_reg_config(multiplier=0.2, adv_step_size=0.05)
-	adv_model = nsl.keras.AdversarialRegularization(model, label_keys=['label'], adv_config=adv_config)
-	adv_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-	return adv_model
+	return model
 
+# Train base model
 model = lenet_sequential(num_classes, input_shape)
+model.compile(optimizer='adam', loss=categorical_crossentropy, metrics=['accuracy'])
 
-# Train and save model
-model.fit({'feature': x_train, 'label': y_train}, epochs=20, verbose=1)
-model.evaluate({'feature': x_test, 'label': y_test})
-# model.save('lenet_reg_20epoch')
+print("Training base model:")
+model.fit(x=x_train, y=y_train_cat, epochs=epochs, verbose=1)
+model.evaluate(x=x_test, y=y_test_cat)
 
+# Train adversarial model
+adv_config = nsl.configs.make_adv_reg_config(multiplier=adv_mult, adv_step_size=adv_step)
+adv_model = nsl.keras.AdversarialRegularization(lenet_sequential(num_classes, input_shape), label_keys=['label'], adv_config=adv_config)
+adv_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+print("Training adversarial model:")
+adv_model.fit({'feature': x_train, 'label': y_train}, epochs=epochs, verbose=1)
+adv_model.evaluate({'feature': x_test, 'label': y_test})
 
 # Attack
-adversarial_x_test = fast_gradient_method(model, x_test, 0.3, np.inf)
+adversarial_x_test = fast_gradient_method(model_fn=model, x=x_test, eps=attack_eps, norm=np.inf)
 
-print(model.evaluate(x=adversarial_x_test, y=y_test))
+print("Attacking base model:")
+print("Accuracy:", model.evaluate(x=adversarial_x_test, y=y_test_cat)[1])
+
+print("Attacking adversarial model:")
+print("Accuracy:", adv_model.evaluate({'feature': adversarial_x_test, 'label': y_test})[2])
 
