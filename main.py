966e826798b21c10076b93ce01162ed6c94e9643
Spencer Hoffman <hoffmans1@students.rowan.edu>
2020-12-14 21:43:12 -0500

    Now trains every model and training noise factor combination in sequence, with a FGSM epsilon of 0.03.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import noise
 epochs = 20
 adv_mult = 0.2
 adv_step = 0.05
-attack_eps = 0.3
+attack_eps = 0.03
 attack_norm = np.inf
 noise_factor = 0.01
 
@@ -22,76 +22,53 @@ x_test_noise_001 = noise.add_noise(x_test, 0.01)
 x_test_noise_005 = noise.add_noise(x_test, 0.05)
 x_test_noise_01 = noise.add_noise(x_test, 0.1)
 
-# Models:
-# lenet_base, lenet_max, lenet_deep5, lenet_max_deep5, lenet_large, lenet_max_large, lenet_deep5_large, lenet_max_deep5_large, lenet_filters2, lenet_max_filters2
-selected_model = load_model.lenet_base
+data_models = []
 
-# Train Data:
-# x_train, x_train_noise_001, x_train_noise_005, x_train_noise_01
-selected_train_data = x_train
+for selected_train_data in [x_train, x_train_noise_001, x_train_noise_005, x_train_noise_01]:
+	for selected_model in [load_model.lenet_base, load_model.lenet_max, load_model.lenet_deep5, load_model.lenet_max_deep5, load_model.lenet_large, load_model.lenet_max_large, load_model.lenet_deep5_large, load_model.lenet_max_deep5_large, load_model.lenet_filters2, load_model.lenet_max_filters2]:
 
-model = train.train(selected_model(), epochs, (selected_train_data, y_train_cat), (x_test, y_test_cat))
-adv_model = train.train_regularize(selected_model(), epochs, adv_mult, adv_step, (selected_train_data, y_train), (x_test, y_test))
+		model = train.train(selected_model(), epochs, (selected_train_data, y_train_cat), (x_test, y_test_cat))
+		adv_model = train.train_regularize(selected_model(), epochs, adv_mult, adv_step, (selected_train_data, y_train), (x_test, y_test))
 
-#print("0.01 Noise in Model", attack.evaluate(model, (x_test_noise_001, y_test_cat)))
-#print("0.01 Noise in Reg Model", attack.evaluate_regularize(adv_model, (x_test_noise_001, y_test)))
-#print("0.05 Noise in Model", attack.evaluate(model, (x_test_noise_005, y_test_cat)))
-#print("0.05 Noise in Reg Model", attack.evaluate_regularize(adv_model, (x_test_noise_005, y_test)))
-#print("0.1 Noise in Model", attack.evaluate(model, (x_test_noise_01, y_test_cat)))
-#print("0.1 Noise in Reg Model", attack.evaluate_regularize(adv_model, (x_test_noise_01, y_test)))
+		x_test_adv = attack.generate_attack(model, x_test, attack_eps, attack_norm)
+		x_test_noise_001_adv = noise.add_noise(x_test_adv, 0.01)
+		x_test_noise_005_adv = noise.add_noise(x_test_adv, 0.05)
+		x_test_noise_01_adv = noise.add_noise(x_test_adv, 0.1)
 
-x_test_adv = attack.generate_attack(model, x_test, attack_eps, attack_norm)
-x_test_noise_001_adv = noise.add_noise(x_test_adv, 0.01)
-x_test_noise_005_adv = noise.add_noise(x_test_adv, 0.05)
-x_test_noise_01_adv = noise.add_noise(x_test_adv, 0.1)
+		data_model_eval = []
 
-#print("Attacked Model", attack.evaluate(model, (x_test_adv, y_test_cat)))
-#print("Attacked Reg Model", attack.evaluate_regularize(adv_model, (x_test_adv, y_test)))
-#print("0.01 Noise in Attacked Model", attack.evaluate(model, (x_test_noise_001_adv, y_test_cat)))
-#print("0.01 Noise in Attacked Reg Model", attack.evaluate_regularize(adv_model, (x_test_noise_001_adv, y_test)))
-#print("0.05 Noise in Attacked Model", attack.evaluate(model, (x_test_noise_005_adv, y_test_cat)))
-#print("0.05 Noise in Attacked Reg Model", attack.evaluate_regularize(adv_model, (x_test_noise_005_adv, y_test)))
-#print("0.1 Noise in Attacked Model", attack.evaluate(model, (x_test_noise_01_adv, y_test_cat)))
-#print("0.1 Noise in Attacked Reg Model", attack.evaluate_regularize(adv_model, (x_test_noise_01_adv, y_test)))
+#		data_model_eval.append(attack.evaluate(model, (selected_train_data, y_train_cat)))
+		data_model_eval.append(attack.evaluate(model, (x_test, y_test_cat)))
+#		data_model_eval.append(attack.evaluate_regularize(adv_model, (selected_train_data, y_train)))
+		data_model_eval.append(attack.evaluate_regularize(adv_model, (x_test, y_test)))
+		data_model_eval.append(attack.evaluate(model, (x_test_noise_001, y_test_cat)))
+		data_model_eval.append(attack.evaluate(model, (x_test_noise_005, y_test_cat)))
+		data_model_eval.append(attack.evaluate(model, (x_test_noise_01, y_test_cat)))
+		data_model_eval.append(attack.evaluate_regularize(adv_model, (x_test_noise_001, y_test)))
+		data_model_eval.append(attack.evaluate_regularize(adv_model, (x_test_noise_005, y_test)))
+		data_model_eval.append(attack.evaluate_regularize(adv_model, (x_test_noise_01, y_test)))
+		data_model_eval.append(attack.evaluate(model, (x_test_adv, y_test_cat)))
+		data_model_eval.append(attack.evaluate_regularize(adv_model, (x_test_adv, y_test)))
+		data_model_eval.append(attack.evaluate(model, (x_test_noise_001_adv, y_test_cat)))
+		data_model_eval.append(attack.evaluate(model, (x_test_noise_005_adv, y_test_cat)))
+		data_model_eval.append(attack.evaluate(model, (x_test_noise_01_adv, y_test_cat)))
+		data_model_eval.append(attack.evaluate_regularize(adv_model, (x_test_noise_001_adv, y_test)))
+		data_model_eval.append(attack.evaluate_regularize(adv_model, (x_test_noise_005_adv, y_test)))
+		data_model_eval.append(attack.evaluate_regularize(adv_model, (x_test_noise_01_adv, y_test)))
 
-base_train = attack.evaluate(model, (selected_train_data, y_train_cat))
-base_test = attack.evaluate(model, (x_test, y_test_cat))
-adv_train = attack.evaluate_regularize(adv_model, (selected_train_data, y_train))
-adv_test = attack.evaluate_regularize(adv_model, (x_test, y_test))
-noise_base_001 = attack.evaluate(model, (x_test_noise_001, y_test_cat))
-noise_base_005 = attack.evaluate(model, (x_test_noise_005, y_test_cat))
-noise_base_01 = attack.evaluate(model, (x_test_noise_01, y_test_cat))
-noise_adv_001 = attack.evaluate_regularize(adv_model, (x_test_noise_001, y_test))
-noise_adv_005 = attack.evaluate_regularize(adv_model, (x_test_noise_005, y_test))
-noise_adv_01 = attack.evaluate_regularize(adv_model, (x_test_noise_01, y_test))
-base_attack = attack.evaluate(model, (x_test_adv, y_test_cat))
-adv_attack = attack.evaluate_regularize(adv_model, (x_test_adv, y_test))
-noise_base_001_attack = attack.evaluate(model, (x_test_noise_001_adv, y_test_cat))
-noise_base_005_attack = attack.evaluate(model, (x_test_noise_005_adv, y_test_cat))
-noise_base_01_attack = attack.evaluate(model, (x_test_noise_01_adv, y_test_cat))
-noise_adv_001_attack = attack.evaluate_regularize(adv_model, (x_test_noise_001_adv, y_test))
-noise_adv_005_attack = attack.evaluate_regularize(adv_model, (x_test_noise_005_adv, y_test))
-noise_adv_01_attack = attack.evaluate_regularize(adv_model, (x_test_noise_01_adv, y_test))
+		data_models.append(data_model_eval)
+		
+		print("#################")
+		for data_model_el in data_model_eval[:-1]:
+			print("%.4f" % data_model_el, end=",")
+		print("%.4f" % data_model_eval[-1])
+		print("#################")
 
-print("################")
-print("# Data to copy #")
-print("################")
-print("%.4f" % base_train, end=",")
-print("%.4f" % base_test, end=",")
-print("%.4f" % adv_train, end=",")
-print("%.4f" % adv_test, end=",")
-print("%.4f" % noise_base_001, end=",")
-print("%.4f" % noise_base_005, end=",")
-print("%.4f" % noise_base_01, end=",")
-print("%.4f" % noise_adv_001, end=",")
-print("%.4f" % noise_adv_005, end=",")
-print("%.4f" % noise_adv_01, end=",")
-print("%.4f" % base_attack, end=",")
-print("%.4f" % adv_attack, end=",")
-print("%.4f" % noise_base_001_attack, end=",")
-print("%.4f" % noise_base_005_attack, end=",")
-print("%.4f" % noise_base_01_attack, end=",")
-print("%.4f" % noise_adv_001_attack, end=",")
-print("%.4f" % noise_adv_005_attack, end=",")
-print("%.4f" % noise_adv_01_attack)
-print("################")
+print("#################")
+print("# Result Matrix #")
+print("#################")
+for data_model in data_models:
+	for data_model_el in data_model[:-1]:
+		print("%.4f" % data_model_el, end=",")
+	print("%.4f" % data_model[-1])
+print("#################")
